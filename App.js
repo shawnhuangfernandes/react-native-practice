20aa2df7f973b38afda8133dace5ee1f59cb9066
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-02-06 17:31:14 -0800

    created folder structure for react-native app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
