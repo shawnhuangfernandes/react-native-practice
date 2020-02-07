dd8da105b82e07017a32850d4210105333612af1
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-02-06 18:01:46 -0800

    learned and tested how to use style objects to apply styling to react native components

--- App.js
+++ App.js
@@ -3,12 +3,29 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <TextInput />
+    <View style={styles.screen}>
+      <View
+        style={styles.inputContainer}
+      >
+        <TextInput
+          placeholder={"Course Goal"}
+          style={styles.textField}
+        />
         <Button title="ADD" />
       </View>
       <View></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textField: { width: "80%", borderBottomColor: "black", borderBottomWidth: 1 }
+});
